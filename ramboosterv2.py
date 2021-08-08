e53f36fd3f2135e3e7b89dbc8c1ed1e95a9e7f1a
OpenRafy <g635943@gmail.com>
2021-08-08 16:10:19 +0200

    added requirements.txt

--- ramboosterv2.py
+++ ramboosterv2.py
@@ -1,1 +1,15 @@
+import colorama
+from termcolor import colored
 
+from PyInquirer import prompt
+
+colorama.init()
+
+is_running = True
+
+colorama.init()
+while is_running:
+
+    print(colored("RAM BOOSTER V2","blue"))
+
+    print(colored("V2.0","red"))
